630203e134ff3efcafb117dbb6e11ae6e077cca0
Maxim Liutsko <maximliutsko@Maxims-MacBook-Pro.local>
2016-12-09 16:52:21 +0200

    add title and field 5

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -4,7 +4,9 @@ import React from 'react';
 
 const Header = (props) => (
   <div className="header-component">
+    <h1>5x5 2 Player Tic-Tac-Toe</h1>
     <select id="field">
+      <option value="5">5</option>
       <option selected value="10">10</option>
       <option value="12">12</option>
       <option value="14">14</option>
